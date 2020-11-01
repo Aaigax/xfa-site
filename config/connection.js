1eaf52dc08e7d2899f92147a0e0bed691e5f362e
Aaiga <ala11_aiga@hotmail.com>
2020-11-02 10:18:32 +1100

    update connection host

--- config/connection.js
+++ config/connection.js
@@ -2,25 +2,25 @@ var mysql = require('mysql');
 
 // AWS Server:
 
-// var connection = mysql.createConnection({
-//    host: 'xfa-site.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
-//    user: 'admin',
-//    password: 'AlatinaX!83',
-//    database: 'db_xfafamilyzone',
-//    port: 3306,
-//    multipleStatements: true
-// });
-
-// Development:
-
 var connection = mysql.createConnection({
-   host: 'xfa-site',
+   host: 'xfa-site.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
    user: 'admin',
    password: 'AlatinaX!83',
    database: 'db_xfafamilyzone',
    port: 3306,
    multipleStatements: true
 });
+
+// Development:
+
+// var connection = mysql.createConnection({
+//    host: 'xfa-site',
+//    user: 'admin',
+//    password: 'AlatinaX!83',
+//    database: 'db_xfafamilyzone',
+//    port: 3306,
+//    multipleStatements: true
+// });
 
 connection.connect(function (err) {
    if (!err) {
