807e374d30170a32a69616647afa628f3b02a182
Aaiga <ala11_aiga@hotmail.com>
2020-11-02 10:02:13 +1100

    update new domain for testing

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,20 @@
 var mysql = require('mysql');
 
+// AWS Server:
+
+// var connection = mysql.createConnection({
+//    host: 'xfa-site.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
+//    user: 'admin',
+//    password: 'AlatinaX!83',
+//    database: 'db_xfafamilyzone',
+//    port: 3306,
+//    multipleStatements: true
+// });
+
+// Development:
+
 var connection = mysql.createConnection({
-   host: 'xfa-site.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
+   host: 'xfa-site',
    user: 'admin',
    password: 'AlatinaX!83',
    database: 'db_xfafamilyzone',
@@ -16,4 +29,5 @@ connection.connect(function (err) {
       console.log("Error while connecting with database");
    }
 });
+
 module.exports = connection;
